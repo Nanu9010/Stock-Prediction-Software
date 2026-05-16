6da10c17aacb8357637ee46f7bae7a2ec94869fb
Kartikraj Nalage <kartikrajnalage9010@gmail.com>
2026-05-16 12:59:23 +0530

    fix: force setuptools installation and add debug logs for pkg_resources

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,6 +2,11 @@
 WSGI config for production deployment
 """
 import os
+try:
+    import pkg_resources
+    print("pkg_resources available")
+except ImportError:
+    print("pkg_resources NOT available")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings.production'))
